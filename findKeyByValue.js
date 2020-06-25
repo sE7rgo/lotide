f3462c9ebd4a2546094573d9b974d6b753f05626
sE7rgo <se7rgo@gmail.com>
2020-06-25 02:27:06 +0000

    looking for a key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const findKeyByValue = function(object, value) {
+  let result;
+  for (let key in object) {
+    if (value === object[key]) {
+      result = key;
+    } else {
+      result = 'undefined';
+    }
+  }
+  return result;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
