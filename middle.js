6ca59e27c6bc135ff0a8ea6a340ff8640d7b1606
sE7rgo <se7rgo@gmail.com>
2020-06-29 20:46:26 +0000

    added dependencies between modules

--- middle.js
+++ middle.js
@@ -1,28 +1,3 @@
-const eqArrays = function(firstArray, secondArray) {
-  let output;
-  for (let i = 0; i < firstArray.length; i++) {
-    firstArray[i] === secondArray[i] ? output = true : output = false;
-  }
-  console.log(output);
-  return output;
-};
-
-
-const assertArraysEqual = function(firstArray, secondArray) {
-  let output;
-  for (let i = 0; i < firstArray.length; i++) {
-
-    if (firstArray[i] === secondArray[i]) {
-      output = `✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`;
-    } else {
-      output = `🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`;
-    }
-  }
-  console.log(output);
-  return output;
-};
-
-
 const middle = function(array) {
   let midArr = [];
   let avgIdx = Math.ceil(array.length / 2) - 1;
@@ -35,5 +10,5 @@ const middle = function(array) {
   }
   return midArr;
 };
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
+
+module.exports = middle;
