0e69e89b5be89175e45537e5411c7f2deffe57ac
sE7rgo <se7rgo@gmail.com>
2020-06-29 20:35:05 +0000

    Added dependencies between modules

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,8 @@
-const eqArrays = function(firstArray, secondArray) {
-  let output;
-  for (let i = 0; i < firstArray.length; i++) {
-
-    if (firstArray[i] === secondArray[i]) {
-      output = `✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`;
-    } else {
-      output = `🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`;
-    }
-  }
-  console.log(output);
-  return output;
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = function(firstArray, secondArray) {
+  eqArrays(firstArray, secondArray) ? 
+  console.log(`✅✅✅ Assertion Passed: ${firstArray} === ${secondArray}`) :
+  console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = assertArraysEqual;
