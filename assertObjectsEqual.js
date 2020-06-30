653bece592a8e51e5e78a2c787a28537c2518ddb
sE7rgo <se7rgo@gmail.com>
2020-06-30 01:48:38 +0000

    Remove node directory

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,7 +1,6 @@
 const eqObjects = require('./index').eqObjects
  
 const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
